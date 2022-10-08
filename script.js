14a97337cebf98ac754472365ff58d91232df74a
unknown <mango__82@hotmail.com>
2022-10-08 18:36:47 +1100

    add function to display book objects

--- script.js
+++ script.js
@@ -4,9 +4,27 @@ function Book(title, author, pages, read){
     this.pages = pages
     this.read = read
 }
+const theHobbit = new Book("The Hobbit", "JRR Tolkien", 242,"Not Read")
+const theMartian = new Book("The Martian", "Jeremey", 124,"Read")
 
-let myLibrary = ["Book1","Book2"];
+let myLibrary = [theHobbit, theMartian];
     //empty array to add books to
+
+const div = document.getElementById("div")
+book = document.createElement("p")
+
+const refresh = document.getElementById("refresh")
+refresh.addEventListener("click", function(){
+    book.innerHTML = ""
+    for (let i = 0; i < myLibrary.length; i++){
+    let myArray = Object.values(myLibrary[i])
+    book.innerHTML += "<br>" + myArray
+    div.appendChild(book)
+    }
+})
+
+console.log(addBookToLibrary())
+console.log(myLibrary)
 
 function addBookToLibrary(){
     //gets user prompt and adds book to array
@@ -24,8 +42,7 @@ function addBookToLibrary(){
 
 
 
+
 // let theHobbit = new Book("The Hobbit", "J.R.R Tolkein", "255 pages", "Not read")
 // console.log(theHobbit.info())
 
-console.log(addBookToLibrary())
-console.log(myLibrary)
